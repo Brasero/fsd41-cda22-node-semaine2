bb0c34af26caf0ad7e51423a999e28cc6ae5b6e9
Brasero <brandonricci4@gmail.com>
2023-11-20 10:36:45 +0100

    part 1

---
+++ expressDiscover/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import {join} from 'node:path'
+
+const cwd = process.cwd() // permet de récupérer le chemin absolu du dossier d'execution
+
+const app = express(); // instancie une application express
+
+
+// Une route est composé de son path "/" et de son handler (req, res) => {...}
+/*
+app.get("/", (req, res) => {
+    const {name, age} = req.query //paramétre de requete (ex: http://localhost:8000/?name=test&age=12)
+
+    console.log(name, age);
+    res.status(200).sendFile(join(cwd, 'index.html'), (err) => { //sendFile permet d'envoyer des fichiers en tant que réponse
+        if (err) res.status(404).send('error')
+    })
+})
+
+app.post("/", (req, res) => {
+    res.send("You're in POST method")
+})
+
+app.get('/hello/15', (req, res) => {
+
+    res.send('second route')
+})
+
+// un paramètre dynamique est déclaré dans le path en étant précedé de ":", il est possible d'en déclarer autant que l'on souhaite
+// il est ensuite possible de les récupérer dans "req.params"
+app.get('/:name/:age', (req, res) => {
+    const {name, age} = req.params
+
+    console.log(name, age)
+    res.send('ok')
+})
+
+ */
+
+
+
+
+app.listen(8000, () => {
+    console.log('Server started at http://localhost:8000')
+})
